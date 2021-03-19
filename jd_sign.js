d2712bce300c4672356aa38dc53e0472373767ff
s9901 <44739953+s9901@users.noreply.github.com>
2021-03-19 16:50:55 +0800

    Update jd_sign.js

--- jd_sign.js
+++ jd_sign.js
@@ -7,10 +7,14 @@ const fs = require('fs')
 const rp = require('request-promise')
 const download = require('download')
 
-// 京东cookie
+// 京东Cookie
 const cookie = process.env.JD_COOKIE
+// 京东Cookie
+const dual_cookie = process.env.JD_DUAL_COOKIE
 // Server酱SCKEY
 const push_key = process.env.PUSH_KEY
+// Server酱SCKEY（Turbo 版）
+const push_key_turbo = process.env.PUSH_KEY_TURBO
 
 // 京东脚本文件
 const js_url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
@@ -41,14 +45,25 @@ Date.prototype.Format = function (fmt) {
   return fmt;
 };
 
+function dateFormat() {
+  var timezone = 8;
+  var GMT_offset = new Date().getTimezoneOffset();
+  var n_Date = new Date().getTime();
+  var t_Date = new Date(n_Date + GMT_offset * 60 * 1000 + timezone * 60 * 60 * 1000);
+  console.log(t_Date)
+  return t_Date.Format('yyyy.MM.dd')
+}
+
 function setupCookie() {
   var js_content = fs.readFileSync(js_path, 'utf8')
   js_content = js_content.replace(/var Key = ''/, `var Key = '${cookie}'`)
+  if (dual_cookie) {
+    js_content = js_content.replace(/var DualKey = ''/, `var DualKey = '${dual_cookie}'`)
+  }
   fs.writeFileSync(js_path, js_content, 'utf8')
 }
 
 function sendNotificationIfNeed() {
-
   if (!push_key) {
     console.log('执行任务结束!'); return;
   }
@@ -57,7 +72,7 @@ function sendNotificationIfNeed() {
     console.log('没有执行结果，任务中断!'); return;
   }
 
-  let text = "京东签到_" + new Date().Format('yyyy.MM.dd');
+  let text = "京东签到_" + dateFormat();
   let desp = fs.readFileSync(result_path, "utf8")
 
   // 去除末尾的换行
@@ -66,6 +81,44 @@ function sendNotificationIfNeed() {
   const options ={
     uri:  `https://sc.ftqq.com/${SCKEY}.send`,
     form: { text, desp },
+    json: true,
+    method: 'POST'
+  }
+
+  rp.post(options).then(res=>{
+    const code = res['errno'];
+    if (code == 0) {
+      console.log("通知发送成功，任务结束！")
+    }
+    else {
+      console.log(res);
+      console.log("通知发送失败，任务中断！")
+      fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+    }
+  }).catch((err)=>{
+    console.log("通知发送失败，任务中断！")
+    fs.writeFileSync(error_path, err, 'utf8')
+  })
+}
+
+function sendNotificationIfNeed_turbo() {
+  if (!push_key_turbo) {
+    console.log('执行任务结束!'); return;
+  }
+
+  if (!fs.existsSync(result_path)) {
+    console.log('没有执行结果，任务中断!'); return;
+  }
+
+  let title = "京东签到_" + dateFormat();
+  let desp = fs.readFileSync(result_path, "utf8")
+
+  // 去除末尾的换行
+  let SCKEY = push_key_turbo.replace(/[\r\n]/g,"")
+
+  const options ={
+    uri:  `https://sctapi.ftqq.com/${SCKEY}.send`,
+    form: { title, desp },
     json: true,
     method: 'POST'
   }
@@ -99,7 +152,10 @@ function main() {
     // 3、执行脚本
     exec(`node '${js_path}' >> '${result_path}'`);
     // 4、发送推送
-    sendNotificationIfNeed() 
+    if(push_key_turbo)
+      sendNotificationIfNeed_turbo()
+    else
+      sendNotificationIfNeed()
   }).catch((err)=>{
     console.log('脚本文件下载失败，任务中断！');
     fs.writeFileSync(error_path, err, 'utf8')
